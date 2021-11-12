bbc7724e6627b0a8eeacc1b3503c80da58d64613
troytam <89545861+troytam@users.noreply.github.com>
2021-11-12 09:01:25 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,11 +1,12 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Student {
 
     public static void createAccount(String firstName, String lastName, String username, String password)
             throws FileNotFoundException {
-        FileOutputStream fos = new FileOutputStream("StudentsAccounts.txt", true);
+        FileOutputStream fos = new FileOutputStream("StudentAccounts.txt", true);
         StringBuilder courses = new StringBuilder();
         PrintWriter pw = new PrintWriter(fos);
         pw.println("Name: " + firstName + " " + lastName);
@@ -13,6 +14,107 @@ public class Student {
         pw.println("Password: " + password);
         pw.println();
         pw.flush();
+    }
+
+    public static ArrayList<String> getAllUsernames(){
+        ArrayList<String> fileContents = new ArrayList<>();
+        ArrayList<String> allUsernames = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("StudentAccounts.txt"))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                fileContents.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        if (fileContents.size() == 0) {
+            return null;
+        } else {
+            for (int i = 0; i < fileContents.size() / 4; i++) {
+                String user = fileContents.get(1 + 4 * i);
+                allUsernames.add(user.substring(10));
+            }
+        }
+        return allUsernames;
+    }
+
+    public static String changeUsername(String oldUsername, String newUsername) throws FileNotFoundException {
+        int usernameExist = 0;
+        StringBuffer buffer = new StringBuffer();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("StudentAccounts.txt"))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                buffer.append(line + " \n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String fileContents = buffer.toString();
+        if (buffer.isEmpty()) {
+             return "No student accounts have been created.";
+        } else {
+            for (int i = 0; i < fileContents.length(); i++) {
+                if (fileContents.contains(oldUsername)) {
+                    fileContents = fileContents.replace(oldUsername, newUsername);
+                    usernameExist = 1;
+                }
+            }
+            if (usernameExist != 1) {
+                return "Your current username does not exist.";
+            }
+            String [] splitContents = fileContents.split("\n");
+            FileOutputStream fos = new FileOutputStream("StudentAccounts.txt", false);
+            PrintWriter pw = new PrintWriter(fos);
+            for (int i = 0; i < splitContents.length; i++) {
+                pw.println(splitContents[i]);
+            }
+            pw.flush();
+        }
+        return "Your username has been changed!";
+    }
+
+    public static String changePassword(String username, String oldPassword, String newPassword) throws FileNotFoundException {
+        int usernameExist = 0;
+        int oldPasswordExist = 0;
+        StringBuffer buffer = new StringBuffer();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("StudentAccounts.txt"))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                buffer.append(line + " \n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String fileContents = buffer.toString();
+        if (buffer.isEmpty()) {
+            return "No student accounts have been created.";
+        } else {
+            for (int i = 0; i < fileContents.length(); i++) {
+                if (fileContents.contains(username)) {
+                    usernameExist = 1;
+                    if (fileContents.contains(oldPassword)) {
+                        fileContents = fileContents.replace(oldPassword, newPassword);
+                        oldPasswordExist = 1;
+                    }
+                }
+            }
+            if (oldPasswordExist != 1) {
+                return "This is not your current password. Please enter your current password correctly.";
+            }
+            if (usernameExist != 1) {
+                return "Your current username does not exist.";
+            }
+            String [] splitContents = fileContents.split("\n");
+            FileOutputStream fos = new FileOutputStream("StudentAccounts.txt", false);
+            PrintWriter pw = new PrintWriter(fos);
+            for (int i = 0; i < splitContents.length; i++) {
+                System.out.println(splitContents[i]);
+                pw.println(splitContents[i]);
+            }
+            pw.flush();
+        }
+        return "Your password has been changed!";
     }
 
     //if program has error, run function to save answers
@@ -69,11 +171,23 @@ public class Student {
             finishedAnswers.add(b);
             b += 2;
         }
-        createAccount("Whats up", "Yall", "hey", "cool");
+        /**
+        createAccount("First", "Last", "hey", "cool");
         writeUnfinishedQuizAnswersToFile("Troy", "Tamura", courseName, "Strings", unfinishedAnswers);
         writeFinishedQuizAnswersToFile("Troy", "Tamura", courseName, "test", unfinishedAnswers, 90);
         writeUnfinishedQuizAnswersToFile("Hello", "World", courseName, "Hello", unfinishedAnswers);
+        //createAccount("Whats up", "Yall", "hey", "cool");
+        */
+        createAccount("Artemii", "IS-Cool", "hey", "nice");
+        //System.out.println(changeUsername("i'm", "bruh"));
+        //System.out.println(changePassword("bruh", "dope", "cool"));
+        ArrayList<String> users = getAllUsernames();
+        for (int i = 0; i < users.size(); i++) {
+            System.out.println(users.get(i));
+        }
     }
 }
 
 //student answers are in an arraylist
+
+//need to get list of usernames to make sure that they aren't the same
