cdae4f2a1985f5a5952be7926045f3256ab28fab
artemiichirkov2 <94003884+artemiichirkov2@users.noreply.github.com>
2021-11-13 16:48:52 -0500

    Update Application.java

--- Application.java
+++ Application.java
@@ -2,8 +2,10 @@
 This class starts the program and asks a user to sign in or register
 *
 In order to save all the registration information, I need Troy's file technology implemented
+*
+Need to make a while loop for main function + quit from the program
 */
-import java.util.*; 
+import java.util.*;
 
 public class Application {
 
@@ -16,12 +18,26 @@ public class Application {
     private static String enterPassword = "Enter your password.";
     private static String takenUsername = "Sorry, this username is already taken. Please enter a different username";
     private static String usernameDoesntExist = "Sorry, there is no user with this username. Please try again or Register.";
+    private static String passwordDoesntMatch = "Sorry, the password you entered doesn't match the username";
+    private static String enterNewPassword = "Please, enter the new password";
+    private static String signInAgain = "Please Sign In again to confirm your password change";
+    private static String confirmQuestion = 
+    "Are you sure that you want to delete your account?\n" +
+    "Press [1] for yes, or [2] for no";
     private static String teacherOrStudentSignIn = 
     "Do you want to sign in as a teacher or as a student?\n" +
     "Pick [1] for teacher or [2] for student.";
     private static String teacherOrStudentRegister = 
     "Do you want to register as a teacher or as a student?\n" +
     "Pick [1] for teacher or [2] for student.";
+    private static String menuTeacher = 
+    "[1] Change your password\n" +
+    "[2] Delete your account\n"; // And other options
+    private static String menuStudent = 
+    "Menu:\n" +
+    "[1] Change your password\n" +
+    "[2] Delete your account\n"; // And other options
+    private static String chooseOne = "Pick a corresponding number to choose an option";
     static ArrayList<Teacher> teachers = new ArrayList<Teacher>();
     static ArrayList<Student> students = new ArrayList<Student>();
     static int usernameStatus = 1; // Checks if there is already a user with that username
@@ -29,7 +45,7 @@ public class Application {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Scanner scanner = new Scanner(System.in);
         System.out.println(teachers);
 
@@ -52,7 +68,7 @@ public class Application {
 
     }
 
-    public static void signIn(Scanner scanner) { // Sign in method either for teachers or students
+    public static void signIn(Scanner scanner) throws Exception{ // Sign in method either for teachers or students
         
         System.out.println("Sign In\n");
         System.out.println(teacherOrStudentSignIn);
@@ -70,8 +86,15 @@ public class Application {
                 {
                     System.out.println(enterPassword);
                     String password = scanner.nextLine();
-                    if (item.getPassword() == password) {
-                        // The list of things that the teacher can do
+                    if (password.equals(item.getPassword())) {
+
+                        System.out.println("Success!\n");
+                        menuTeacher(username, scanner);
+
+                    }
+                    else
+                    {
+                        System.out.println(passwordDoesntMatch);
                     }
                 } 
                 else
@@ -92,8 +115,15 @@ public class Application {
                 {
                     System.out.println(enterPassword);
                     String password = scanner.nextLine();
-                    if (item.getPassword() == password) {
-                        // The list of things that the student can do
+                    if (password.equals(item.getPassword())) {
+
+                        System.out.println("Success!\n");
+                        menuStudent(username, scanner);
+
+                    }
+                    else
+                    {
+                        System.out.println(passwordDoesntMatch);
                     }
                 } 
                 else
@@ -108,7 +138,7 @@ public class Application {
         }
     }
 
-    public static void register(Scanner scanner) { // Allows user to register and then Sign In
+    public static void register(Scanner scanner) throws Exception{ // Allows user to register and then Sign In
 
         System.out.println("Register\n");
         System.out.println(teacherOrStudentRegister);
@@ -175,7 +205,133 @@ public class Application {
             System.out.println(incorrectAnswer);
         }
 
+    }
 
+    public static void changePasswordTeacher(String username, Scanner scanner) throws Exception{
+        for (Teacher item: teachers) {
+            if (username.equals(item.getUsername()))
+            {
+                System.out.println(enterNewPassword);
+                scanner.nextLine();
+                String newPassword = scanner.nextLine();
+                item.setPassword(newPassword);
+                System.out.println(signInAgain);
+                signIn(scanner);
+            }
+        }
+    }
+
+    public static void changePasswordStudent(String username, Scanner scanner) throws Exception{
+        for (Student item: students) {
+            if (username.equals(item.getUsername()))
+            {
+                System.out.println(enterNewPassword);
+                scanner.nextLine();
+                String newPassword = scanner.nextLine();
+                item.setPassword(newPassword);
+                System.out.println(signInAgain);
+                signIn(scanner);
+            }
+        }
+    }
+
+    public static void deleteAccountTeacher(String username, Scanner scanner) throws Exception{
+        try {
+            for (Teacher item: teachers) {
+                if (username.equals(item.getUsername()))
+                {
+                    System.out.println(confirmQuestion);
+                    int confirmation = scanner.nextInt();
+                    if (confirmation == 1) 
+                    {
+                        teachers.remove(item);
+                        System.out.println("Account deleteded!\n");
+                        register(scanner);
+                    }
+                    else if (confirmation == 2) 
+                    {
+                        menuTeacher(username, scanner);
+                    }
+                    else 
+                    {
+                        System.out.println(incorrectAnswer);
+                        menuTeacher(username, scanner);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteAccountStudent(String username, Scanner scanner) throws Exception{
+        System.out.println(confirmQuestion);
+        int confirmation = scanner.nextInt();
+        for (Student item: students) {
+            if (username.equals(item.getUsername()))
+            {
+                if (confirmation == 1) 
+                {
+                    students.remove(item);
+                    System.out.println("Account deleted!\n");
+                    register(scanner);
+                }
+                else if (confirmation == 2) 
+                {
+                    menuStudent(username, scanner);
+                }
+                else 
+                {
+                    System.out.println(incorrectAnswer);
+                    menuStudent(username, scanner);
+                }
+            }
+        }
+    }
+
+    public static void menuTeacher(String username, Scanner scanner) throws Exception{
+        try {
+            System.out.println(menuTeacher);
+            System.out.println(chooseOne);
+            int choice = scanner.nextInt();
+
+            switch(choice) {
+                case 1:
+                changePasswordTeacher(username, scanner);
+                break;
+
+                case 2:
+                deleteAccountTeacher(username, scanner);
+                break;
+
+                default:
+                System.out.println(incorrectAnswer);
+                break;
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    }
+
+    public static void menuStudent(String username, Scanner scanner) throws Exception{
+        System.out.println(menuStudent);
+        System.out.println(chooseOne);
+        int choice = scanner.nextInt();
+
+        switch(choice) {
+            case 1:
+            changePasswordStudent(username, scanner);
+            break;
+
+            case 2:
+            deleteAccountStudent(username, scanner);
+            break;
+
+            default:
+            System.out.println(incorrectAnswer);
+            break;
+        }
     }
     
 }
+
