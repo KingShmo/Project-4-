0b8ff49706fbdf3a91bab28a7707ae455c78a56c
ZuhairAlmansouri <89707863+ZuhairAlmansouri@users.noreply.github.com>
2021-11-15 13:31:33 -0500

    Update TimeStamp.java

--- TimeStamp.java
+++ TimeStamp.java
@@ -1,7 +1,14 @@
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-
-
+/**
+ * TimeStamp class
+ * 
+ * Prints a timestamp for a quiz
+ *
+ * @author Anish 
+ * 
+ * @version November 15, 2021
+ */
 public class TimeStamp {
 
     private static final SimpleDateFormat yearMonthDaySpaceHoursMinutesSeconds = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
