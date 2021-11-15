6df0d3d4f96e1cb87307181cfd8d2782c18dea85
ZuhairAlmansouri <89707863+ZuhairAlmansouri@users.noreply.github.com>
2021-11-15 15:41:30 -0500

    Update Course.java

--- Course.java
+++ Course.java
@@ -27,24 +27,37 @@ public class Course {
      */
     private int enrollmentCapacity;
     /**
-     * Array list of students in the course
+     * students enrolled
      */
     private ArrayList<Student> studentsInThisCourse;
-
+    /**
+     * QuizArchive with quizzes
+     */
     QuizArchive quizArchive = new QuizArchive();
 
-    
+    /**
+     * get students
+     */
     public ArrayList<Student> getStudentsInThisCourse() {
         return this.studentsInThisCourse;
     }
-    /**
-     * Returns the array list of students in the course
-     */
 
+    /**
+     * sets students
+     * @param studentsInThisCourse = students to be set
+     */
     public void setStudentsInThisCourse(ArrayList<Student> studentsInThisCourse) {
         this.studentsInThisCourse = studentsInThisCourse;
     }
 
+    /**
+     * Allocates a new course object with its title, assigned teacher and enrollment count.
+     *
+     * @param name = course title.
+     * @param enrollmentCapacity = Enrollment capacity of the course
+     * @param teacher = teacher to be assigned
+     * @throws InvalidCourseException = throws an exception when there is 0 enrollment capacity for the course.
+     */
     public Course(String name, Teacher teacher, int enrollmentCapacity) throws InvalidCourseException {
         this.name = name;
         this.courseTeacher = teacher;
@@ -54,6 +67,15 @@ public class Course {
             throw new InvalidCourseException("A course should have a minimum student enrollment of 1!");
     }
 
+    /**
+     * Allocates a new course object with its title, assigned teacher and enrollment count.
+     *
+     * @param name = course title.
+     * @param enrollmentCapacity = Enrollment capacity of the course
+     * @param teacher = teacher to be assigned
+     * @param studentsInThisCourse = students to be added for the course
+     * @throws InvalidCourseException = throws an exception when there is 0 enrollment capacity for the course.
+     */
     public Course(String name, Teacher teacher, int enrollmentCapacity, ArrayList<Student> studentsInThisCourse) throws InvalidCourseException {
         this(name, teacher, enrollmentCapacity);
         this.studentsInThisCourse = studentsInThisCourse;
@@ -61,54 +83,67 @@ public class Course {
             throw new InvalidCourseException("A course should have a minimum student enrollment capacity of 1!");
     }
 
-    /*
-     * Allocates a new course object with its title, assigned teacher and enrollment count.
-     *
-     * @param name = course title.
-     * @param courseTeacher = Teacher assigned to this course.
-     * @param enrollmentCapacity = Enrollment capacity of the course
-     * @param students = Students enrolled in the course
-     * @throws InvalidCourseException = throws an exception when there is 0 enrollment capacity for the course.
+    /**
+     * adds a student
+     * @param student = student to be added
      */
-
-
     public void addAStudentToTheCourse(Student student) {
         studentsInThisCourse.add(student);
     }
 
+    /**
+     * get name
+     * @return name
+     */
     public String getName() {
         return this.name;
     }
-    /**
-     * Returns the name of the course
-     */
 
+    /**
+     * get courseTeacher
+     * @return courseTeacher
+     */
     public Teacher getCourseTeacher() {
         return this.courseTeacher;
     }
-    /**
-     * Returns the teacher for this course
-     */
 
+    /**
+     * get enrollmentCapacity
+     * @return enrollmentCapacity
+     */
     public int getEnrollmentCapacity() {
         return this.enrollmentCapacity;
     }
-    /**
-     * Returns the enrollment capacity the course
-     */
 
+    /**
+     * sets courseTeacher
+     * @param newCourseTeacher = new teacher
+     */
     public void setCourseTeacher(Teacher newCourseTeacher) {
         this.courseTeacher = newCourseTeacher;
     }
 
+    /**
+     * sets enrollmentCapacity
+     * @param newEnrollmentCapacity = new enrollmentCapacity
+     */
     public void setEnrollmentCapacity(int newEnrollmentCapacity) {
         this.enrollmentCapacity = newEnrollmentCapacity;
     }
 
+    /**
+     * sets name
+     * @param newName = new name
+     */
     public void setName(String newName) {
         this.name = newName;
     }
 
+    /**
+     * calls the quiz menu
+     * @throws InvalidQuizException = thrown when appropriate
+     * @throws FileNotFoundException = thrown when appropriate
+     */
     public void callTheQuizFunction() throws InvalidQuizException, FileNotFoundException {
         TheQuizFunction.main(quizArchive);
 
