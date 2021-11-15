06ca623a7abfa11c944eb91bec072ece33ce1c06
ZuhairAlmansouri <89707863+ZuhairAlmansouri@users.noreply.github.com>
2021-11-15 15:47:10 -0500

    Update InvalidCourseException.java

--- InvalidCourseException.java
+++ InvalidCourseException.java
@@ -1,9 +1,23 @@
+/**
+ * InvalidCourseException
+ * <p>
+ * throws an exception whenever a course is invalid
+ *
+ * @author Anushka,
+ * @version November 15, 2021
+ */
 public class InvalidCourseException extends Exception {
 
+    /**
+     * constructs the class
+     */
     public InvalidCourseException() {
         super();
     }
 
+    /**
+     * constructs the class with a message
+     */
     public InvalidCourseException(String message) {
         super(message);
     }
