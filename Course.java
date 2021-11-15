f6bbf5b30c8c40095bd3319c55629fdfd5e96e2f
Anushka Nilangekar <73744292+AnushkaSn@users.noreply.github.com>
2021-11-15 12:59:04 -0500

    Latest file (new constructor, getter and setter)

---
+++ Course.java
@@ -0,0 +1,101 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Course class
+ * <p>
+ * A representation for one course that a certain number of students
+ * can access.
+ *
+ * @author Anushka Nilangekar
+ * @version November 15, 2021
+ */
+public class Course {
+    /**
+     * Title of the course
+     */
+    private String name;
+    /**
+     * Teacher assigned to this course
+     */
+    private Teacher courseTeacher;
+    /**
+     * Enrollment capacity of the course
+     */
+    private int enrollmentCapacity;
+
+    private ArrayList<Student> studentsInThisCourse;
+
+    QuizArchive quizArchive = new QuizArchive();
+
+    public ArrayList<Student> getStudentsInThisCourse() {
+        return this.studentsInThisCourse;
+    }
+
+    public void setStudentsInThisCourse(ArrayList<Student> studentsInThisCourse) {
+        this.studentsInThisCourse = studentsInThisCourse;
+    }
+
+    public Course(String name, Teacher teacher, int enrollmentCapacity) throws InvalidCourseException {
+        this.name = name;
+        this.courseTeacher = teacher;
+        this.enrollmentCapacity = enrollmentCapacity;
+        studentsInThisCourse = new ArrayList<>();
+        if (enrollmentCapacity < 1)
+            throw new InvalidCourseException("A course should have a minimum student enrollment of 1!");
+    }
+
+    public Course(String name, Teacher teacher, int enrollmentCapacity, ArrayList<Student> studentsInThisCourse) throws InvalidCourseException {
+        this(name, teacher, enrollmentCapacity);
+        this.studentsInThisCourse = studentsInThisCourse;
+        if (enrollmentCapacity < 1)
+            throw new InvalidCourseException("A course should have a minimum student enrollment capacity of 1!");
+    }
+
+    /*
+     * Allocates a new course object with its title, assigned teacher and enrollment count.
+     *
+     * @param name = course title.
+     * @param courseTeacher = Teacher assigned to this course.
+     * @param enrollmentCapacity = Enrollment capacity of the course
+     * @param students = Students enrolled in the course
+     * @throws InvalidCourseException = throws an exception when there is 0 enrollment capacity for the course.
+     */
+
+
+    public void addAStudentToTheCourse(Student student) {
+        studentsInThisCourse.add(student);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Teacher getCourseTeacher() {
+        return this.courseTeacher;
+    }
+
+    public int getEnrollmentCapacity() {
+        return this.enrollmentCapacity;
+    }
+
+    public void setCourseTeacher(Teacher newCourseTeacher) {
+        this.courseTeacher = newCourseTeacher;
+    }
+
+    public void setEnrollmentCapacity(int newEnrollmentCapacity) {
+        this.enrollmentCapacity = newEnrollmentCapacity;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public void callTheQuizFunction() throws InvalidQuizException, FileNotFoundException {
+        TheQuizFunction.main(quizArchive);
+
+    }
+}
