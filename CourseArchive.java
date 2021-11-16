ba30962ec1aabf6f93159579ceb56d8cb1a86342
ZuhairAlmansouri <89707863+ZuhairAlmansouri@users.noreply.github.com>
2021-11-16 14:59:03 -0500

    Update CourseArchive.java

--- CourseArchive.java
+++ CourseArchive.java
@@ -16,21 +16,24 @@ public class CourseArchive {
     /**
      * all courses
      */
-    private ArrayList<Course> courses = new ArrayList<Course>();
+    private ArrayList<Course> courses;
     /**
      * courses that are read
      */
-    private ArrayList<Course> coursesAfterReading = new ArrayList<Course>();
+    private ArrayList<Course> coursesAfterReading;
     /*
      * all courses (static)
      */
-    private static ArrayList<Course> allCourses = new ArrayList<Course>();
+    private static ArrayList<Course> allCourses;
 
     /**
      * contructs courses
      * @throws InvalidCourseException = thrown when appropriate
      */
     public CourseArchive() throws InvalidCourseException {
+        allCourses = new ArrayList<>();
+        coursesAfterReading = new ArrayList<>();
+        courses = new ArrayList<>();
         coursesAfterReading = Teacher.readAllCourses();
         if (coursesAfterReading != null) {
             courses = coursesAfterReading;
@@ -52,12 +55,11 @@ public class CourseArchive {
     }
     //adds a course to the courseArchive
     public void addCourses(Course course) {
-        courses.add(course);
         allCourses.add(course);
     }
     //method that allows teacher to see all the courses
     public ArrayList<Course> getCourses() {
-        return courses;
+        return allCourses;
     }
     //method which allows a teacher to delete an unwanted course
     public void deleteACourse(String titleOfTheCourse) {
